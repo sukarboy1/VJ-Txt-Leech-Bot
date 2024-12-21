3689ab8d097e9cc022023a45aae4f760cba8cbdd
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-12-21 08:50:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,8 @@ bot = Client(
     "bot",
     api_id=API_ID,
     api_hash=API_HASH,
-    bot_token=BOT_TOKEN)
+    bot_token=BOT_TOKEN,
+    ADMINS=ADMINS)
 
 
 @bot.on_message(filters.command(["start"])&(filters.chat(ADMINS)))
