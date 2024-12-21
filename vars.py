1bf27803cfd56b69253635d90c80689a4b84f2cf
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-12-21 08:43:29 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -7,12 +7,5 @@ from os import environ
 API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
+ADMINS = environ.get("ADMINS", "")
 
-try:
-    ADMINS=[]
-    #auth_users id - 1234736828 1375282662
-    for x in (os.environ.get("ADMINS", "").split()):
-        ADMINS.append(int(x))
-except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
-ADMINS.append(OWNER)
