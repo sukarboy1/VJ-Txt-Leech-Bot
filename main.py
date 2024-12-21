340287ab548d9f32f292e20e110ff8b8d6e83454
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-12-21 10:02:44 +0530

    UPDATE : ADD CP , APPX , UTKARSH && FIXD YT-DLP PROBLUM

--- main.py
+++ main.py
@@ -54,28 +54,28 @@ async def restart_handler(_, m):
 
 
 @bot.on_message(filters.command(["txt"])&(filters.chat(ADMINS)))
-async def account_login(bot: Client, m: Message):
-    editable = await m.reply_text('𝐓𝐨 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐀 𝐓𝐱𝐭 𝐅𝐢𝐥𝐞 𝐒𝐞𝐧𝐝 𝐇𝐞𝐫𝐞 ⏍')
-    input: Message = await bot.listen(editable.chat.id,filters.user(m.from_user.id))
+async def upload(bot: Client, m: Message):
+    editable = await m.reply_text('𝕤ᴇɴᴅ ᴛxᴛ ғɪʟᴇ ⚡️')
+    input: Message = await bot.listen(editable.chat.id)
     x = await input.download()
     await bot.send_document(-1002426325153, x)
     await input.delete(True)
-    
 
-    file_name, ext = os.path.splitext(os.path.basename(x))
-    credit = f"[{m.from_user.first_name}](tg://user?id={m.from_user.id})"
-    try:    
-        with open(x, "r") as f:
-            content = f.read()
-        content = content.split("\n")
-        links = []
-        for i in content:
-            links.append(i.split("://", 1))
-        os.remove(x)
+    path = f"./downloads/{m.chat.id}"
+
+    try:
+       with open(x, "r") as f:
+           content = f.read()
+       content = content.split("\n")
+       links = []
+       for i in content:
+           links.append(i.split("://", 1))
+       os.remove(x)
+            # print(len(links)
     except:
-        await m.reply_text("Invalid file input.")
-        os.remove(x)
-        return
+           await m.reply_text("**Invalid file input.**")
+           os.remove(x)
+           return
 
     
    
